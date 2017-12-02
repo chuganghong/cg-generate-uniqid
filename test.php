fafa83d87acc5bfc04b0867fea00760de8fa0ed4
cg <chuganghong@qq.com>
2017-12-03 00:42:02 +0800

    test

--- test.php
+++ test.php
@@ -14,5 +14,7 @@ require_once 'GlobalIdGenerator.php';
 $generator = new GlobalIdGenerator(4,2,7,6,3);
 $generator->test();
 echo '<hr>';
-$id = $generator->getGlobalId(2, 1, 3, 6, 'cg');
+$id = $generator->getGlobalId(2, 1, 3, 6, 'cghongw');
 var_dump($id);
+
+file_put_contents('id.log', $id . "\n", FILE_APPEND);
